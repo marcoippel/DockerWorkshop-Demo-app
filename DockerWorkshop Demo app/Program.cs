cbe58b27ac549318d4d4a9edb22ac796da91c402
Marco Ippel <marco@ippelsoft.nl>
2020-09-07 09:44:29 +0200

    Add project files.

---
+++ DockerWorkshop Demo app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DockerWorkshop_Demo_app
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine(args[0]);
+            }
+        }
+    }
+}
